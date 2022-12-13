e16a88e472b3060441ce5b00645e63416d162f0a
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-12-13 23:20:29 +0100

    Finished first section on mobile

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const body = document.querySelector("body");
 let isMenuOpen = false;
 
 const openNav = () => {
-    sidenav.style.width = "250px";
-    menuBtnIcon.src = "./assets/images/icon-menu-close.svg";
-    isMenuOpen = !isMenuOpen;
-    body.style.backgroundColor = "rgba(0,0,0,0.2)";
-    body.style.transition = "0.5s"
-}
-  
-const closeNav = () => {
-    sidenav.style.width = "0";
-    menuBtnIcon.src = "./assets/images/icon-menu.svg";
-    isMenuOpen = !isMenuOpen;
-    body.style.backgroundColor = "hsl(36, 100%, 99%)";
-    body.style.transition = "0.5s"
-  }
+  sidenav.style.width = "250px";
+  sidenav.style.paddingLeft = "2rem";
+  menuBtnIcon.src = "./assets/images/icon-menu-close.svg";
+  isMenuOpen = !isMenuOpen;
+  body.style.backgroundColor = "rgba(0,0,0,0.2)";
+  body.style.transition = "0.5s";
+};
 
-menuBtn.addEventListener("click", () =>
-  isMenuOpen ? closeNav() : openNav()
-);
+const closeNav = () => {
+  sidenav.style.width = "0";
+  sidenav.style.paddingLeft = "0";
+  menuBtnIcon.src = "./assets/images/icon-menu.svg";
+  isMenuOpen = !isMenuOpen;
+  body.style.backgroundColor = "hsl(36, 100%, 99%)";
+  body.style.transition = "0.5s";
+};
+
+menuBtn.addEventListener("click", () => (isMenuOpen ? closeNav() : openNav()));
